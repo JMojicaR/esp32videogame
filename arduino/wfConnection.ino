f4a6d200b546114dcecbe37b6c434420922b1ca7
JMojicaR <jrodmoj@gmail.com>
2024-06-13 00:13:38 -0600

    Adding code files for esp32 wi-fi connection

---
+++ arduino/wfConnection.ino
@@ -0,0 +1,44 @@
+#include <WiFi.h>
+
+WiFiServer server(2020);
+const byte led_pin = 5;
+const char* ssid = "your wifi ssid";
+const char* password = "your wifi password";
+
+void setup()
+{
+    pinMode(led_pin, OUTPUT);
+    Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+    delay(2000);
+    Serial.println("Conectando a red wifi...");
+    WiFi.begin(ssid, password);
+    while(WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
+    }
+    Serial.print("IP asigned: ");
+    Serial.print(WiFi.localIP());
+    server.begin();
+}
+
+void loop()
+{
+    WiFiClient client = server.available();
+    while(client.connected())
+    {
+        if(client.available() > 0)
+        {
+            char command = client.read();
+            if(command == 'A')
+            {
+                digitalWrite(led_pin, HIGH);
+            }
+            else
+            {
+                digitalWrite(led_pin, LOW);
+            }
+        }
+    }
+}
